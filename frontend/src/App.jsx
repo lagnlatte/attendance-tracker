0fde4c812fba01dc41ddc325bb0ebc4e60437f31
ale <tomescualexandramirela@gmail.com>
2025-12-30 12:39:18 +0200

    frontend(home): create home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from "./pages/Login";
 import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import Confirm from './pages/Confirm'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/confirm" element={<Confirm />}></Route>
     </Routes>
   )
 }
